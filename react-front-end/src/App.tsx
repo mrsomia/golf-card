9f6b1ce5ea9a3f5682a1d4622a321d020005657d
Sachin Somia <sachinsomia@gmail.com>
2022-11-10 23:22:43 +0000

    Feat: Adds landing/Join Room Page/Routing

--- react-front-end/src/App.tsx
+++ react-front-end/src/App.tsx
@@ -1,34 +1,127 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { createBrowserRouter, Link, useParams, useLocation, RouterProvider } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+    // useEffect(() => {
+    //   const socket = io("wss://localhost:8080")
+    //   socket.on('connection', () => {
+    //     console.log(socket.id)
+    //   })
+    // }, [])
+    // const router = createBrowserRouter([
+    //   {
+    //     path: "/",
+    //     element: <div>Hello world!</div>,
+    //   },
+    // ])
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <HomePage />,
+        },
+        {
+            path: "create-room",
+            element: <JoinRoomPage />,
+        },
+        {
+            path: "create-room/:roomId",
+            element: <JoinRoomPage />,
+        },
+        {
+            path: "join-room",
+            element: <JoinRoomPage />,
+        },
+        {
+            path: "join-room/:roomId",
+            element: <JoinRoomPage />,
+        },
+    ]); 
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+
+    return <RouterProvider router={router} />
 }
 
 export default App
+
+interface ButtonProps { 
+    text: string;
+    addClasses?: string
+}
+
+function Button({text, addClasses } : ButtonProps) {
+  return(
+      <button 
+        className={`p-2 px-4 rounded-xl w-40 bg-orange-600 hover:bg-orange-800
+            text-stone-100 ${addClasses}`}
+      >
+        {text}
+      </button>
+  )
+}
+
+function HomePage() {
+    return <div
+        className='flex flex-col justify-center items-center w-screen min-h-screen
+        bg-gray-800 text-stone-300 gap-8'
+    > 
+        <h1 className='text-5xl font-bold pa8 my-8'>Golf Card</h1>
+        <Link to="/create-room">
+            <Button text={"Create Room"} />
+        </Link>
+        <Link to="/join-room">
+            <Button text={"Join Room"} />
+        </Link>
+    </div>
+}
+
+function JoinRoomPage() {
+    const location = useLocation()
+    const createMode = location.pathname.startsWith("/create-room")
+    const { roomId } = useParams()
+    const [ roomID, setRoomID ] = useState(roomId ?? "")
+    const [ userName, setUserName ] = useState("")
+    return <div
+            className='flex flex-col justify-center items-center w-screen
+            min-h-screen bg-gray-800 text-stone-300 gap-8'
+            > 
+        <h1 className='text-5xl font-bold pa8 my-8'>
+            { createMode ? "Create Room" : "Join Room" }
+        </h1>
+        <form 
+            className='flex flex-col justify-center items-center text-center
+            gap-3'
+        >
+            { !createMode && <>
+                <label 
+                    htmlFor="RoomID"
+                    className='text-lg'
+                >
+                    Room ID
+                </label>
+                <input 
+                    type='text'
+                    value={roomID} 
+                    id="RoomID" 
+                    onChange={e => setRoomID(e.target.value)}
+                    className='text-black p-2 px-4 w-60'
+                    />
+                </>
+            }
+            <label 
+                htmlFor="userName"
+                className='text-lg'
+            >
+                Name
+            </label>
+            <input 
+                type='text'
+                value={userName} 
+                id="userName" 
+                onChange={e => setUserName(e.target.value)}
+                className='text-black p-2 px-4 w-60'
+            />
+
+            <Button addClasses={"my-16"} text='Join'/>
+        </form>
+    </div>
+}
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,24 @@
-import express from 'express'
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const port = process.env.PORT || 8080
 
-const app = express()
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000"]
+  }
+});
 
-app.get('/', (req, res) => res.json({ message: "hellow world" }))
+io.on("connection", (socket) => {
+  console.log(socket.id)
+  socket.on('update-state', (state) => {
+    socket.broadcast.emit("update-state", state)
+  })
+});
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`))
+app.get('/', (req, res) => res.json({ message: "hello world" }))
+
+httpServer.listen(port, () => console.log(`Server is listening on port ${port}`));
