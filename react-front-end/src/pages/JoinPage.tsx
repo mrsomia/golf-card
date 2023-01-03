d4caa3a466b4bb1ae1f4603896eae767aa3ee17d
Sachin Somia <sachinsomia@gmail.com>
2023-01-03 19:19:42 +0000

    Fix: makes get room name call to server

--- react-front-end/src/pages/JoinPage.tsx
+++ react-front-end/src/pages/JoinPage.tsx
@@ -1,14 +1,45 @@
 import { useState } from 'react'
-import { useLocation, useParams } from 'react-router-dom'
-import Button from '../components/Button'
+import { useLocation, useParams, useNavigate } from 'react-router-dom'
 
 
 function JoinRoomPage() {
     const location = useLocation()
+    const navigate = useNavigate()
     const createMode = location.pathname.startsWith("/create-room")
     const { roomId } = useParams()
     const [ roomID, setRoomID ] = useState(roomId ?? "")
     const [ userName, setUserName ] = useState("")
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    // Save the username locally
+    e.preventDefault()
+    if (!userName) {
+      console.error("No username present")
+      return
+    }
+    localStorage.setItem('username', userName)
+    if (createMode) {
+      // creates a Room  
+      try {
+        const r = await fetch(`${import.meta.env.VITE_SERVER_URL}/create-room`, {
+          method: 'POST',
+          body: JSON.stringify({
+            user: {
+              name: userName
+            }
+          })
+        })
+        const res = await r.json()
+
+        console.log(res)
+        // TODO: Validate here with zod
+      } catch (e) {
+        // TODO: Handles errors
+        console.error(e)
+      }
+    }
+  }
+
     return <div
             className='flex flex-col justify-center items-center w-screen
             min-h-screen bg-gray-800 text-stone-300 gap-8'
@@ -19,11 +50,11 @@ function JoinRoomPage() {
         <form 
             className='flex flex-col justify-center items-center text-center
             gap-3'
+            onSubmit={handleSubmit}
         >
-            { !createMode && <>
                 <label 
                     htmlFor="RoomID"
-                    className='text-lg'
+                    className={`text-lg ${createMode ? 'hidden' : ''}`}
                 >
                     Room ID
                 </label>
@@ -32,10 +63,8 @@ function JoinRoomPage() {
                     value={roomID} 
                     id="RoomID" 
                     onChange={e => setRoomID(e.target.value)}
-                    className='text-black p-2 px-4 w-60'
+                    className={`text-black p-2 px-4 w-60 ${createMode ? 'hidden' : ''}`}
                     />
-                </>
-            }
             <label 
                 htmlFor="userName"
                 className='text-lg'
@@ -50,7 +79,13 @@ function JoinRoomPage() {
                 className='text-black p-2 px-4 w-60'
             />
 
-            <Button addClasses={"my-16"} text='Join'/>
+            <input
+              type="submit"
+              value={createMode ? "Create Room" : "Join"}
+              className={`p-2 px-4 rounded-xl w-40 bg-orange-600
+                hover:bg-orange-800 text-stone-100 my-16`}
+            />
+
         </form>
     </div>
 }
