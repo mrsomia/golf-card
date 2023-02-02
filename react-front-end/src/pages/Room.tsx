93414fd26ddb49be2ff3bf943499e3ba413c50fc
Sachin Somia <sachinsomia@gmail.com>
2023-02-02 02:00:39 +0000

    Feat: Creates horizontal table

--- react-front-end/src/pages/Room.tsx
+++ react-front-end/src/pages/Room.tsx
@@ -84,17 +84,87 @@ function Room() {
     }
   }
 
-    return <>
-        <div
-          className='flex flex-col justify-center items-center w-screen min-h-screen
-          bg-gray-800 text-stone-300 gap-8'
-        >
-          <h1 className='text-xl md:text-4xl font-extrabold md:font-bold pa8 my-8 text-center'>Room: {`${roomName}`}</h1>
-          <span className="text-lg font-medium m-4">Connected: { '' + isConnected }</span>
-          <span className="text-lg font-medium m-4">Last pong: { lastPong || "-" }</span>
-          <Button onClick={ sendPing } text="Send Ping" />
-        </div>
-      </>
+  const placeholderScores = {
+    holes: [
+      {
+        number: 1,
+        par: 2
+      },
+      {
+        number: 2,
+        par: 5
+      },
+      {
+        number: 3,
+        par: 3
+      },
+      {
+        number: 4,
+        par: 6
+      },
+      {
+        number: 5,
+        par: 9
+      },
+      {
+        number: 6,
+        par: 8
+      },
+    ],
+    players: [
+      {
+        name: 'John',
+        scores: [ 3, 5, 3, 7, 8, 7]
+      },
+      {
+        name: 'Sam',
+        scores: [ 4, 4, 5, 6, 9, 8]
+      },
+    ],
+  }
+
+  return <>
+    <div
+      className='flex flex-col justify-center items-center w-screen min-h-screen
+      bg-gray-800 text-stone-300 gap-8'
+    >
+      <h1 className='text-2xl md:text-3xl font-extrabold md:font-bold pa8 my-8 text-center'>Room: {`${roomName}`}</h1>
+
+      <div className="relative overflow-x-scroll">
+        <table className="text-center">
+          <thead>
+            <tr className="text-lg">
+              <th className="p-2">Hole</th>
+              {placeholderScores.holes.map(hole => (
+                <th className="p-1 px-2">{hole.number}</th>
+              ))}
+              <th className="p-2">Total</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {placeholderScores.players.map(player => (
+              <tr className="text-lg">
+                <td>{player.name}</td>
+                {player.scores.map(score => (
+                  <td>{score}</td>
+                ))}
+                <td>
+                  {player.scores
+                  .filter(num => num != null)
+                  .reduce((p,v) =>  p + v, 0)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <span className="text-lg font-medium m-4">Connected: { '' + isConnected }</span>
+      <span className="text-lg font-medium m-4">Last pong: { lastPong || "-" }</span>
+      <Button onClick={ sendPing } text="Send Ping" />
+    </div>
+    </>
 }
 
 export default Room
