4d1e8094e4de8d0d3aa840442e903663a407c20f
Sachin Somia <sachinsomia@gmail.com>
2023-02-25 23:05:31 +0000

    refactor(scoreQueryFn): Formatting and typo fix

--- react-front-end/src/utils/room-utils.ts
+++ react-front-end/src/utils/room-utils.ts
@@ -24,25 +24,26 @@ export const scoreSchema = z.object({
 })
 
 export const scoreQueryFn = async (username: string, roomName: string) => {
-      try{
-        const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/room-score/${roomName}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body : JSON.stringify({ username }),
-        })
-        const r = await res.json()
-        console.log({ res, r })
-        if (res.ok) {
-          const validatedScore = scoreSchema.parse(r)
-          return validatedScore
-        } else {
-          throw new Error(`Unable ot complete fetch, response status was: ${res.status}`)
-        }
-      } catch (e) {
-        const err = e as Error
-        console.error(e)
-        throw err
-      }
+  try{
+    const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/room-score/${roomName}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body : JSON.stringify({ username }),
+    })
+    const r = await res.json()
+    console.log({ res, r })
+    if (res.ok) {
+      const validatedScore = scoreSchema.parse(r)
+      return validatedScore
+    } else {
+      throw new Error(`Unable to complete fetch, response status was: ${res.status}`)
     }
+  } catch (e) {
+    const err = e as Error
+    console.error(e)
+    throw err
+  }
+}
+    
