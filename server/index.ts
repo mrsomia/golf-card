9d270b5101074cd95065bfcbabae6488e55e39fd
Sachin Somia <sachinsomia@gmail.com>
2023-03-04 15:48:58 +0000

    Refactor(server): removes unused socket.io code

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { createServer } from "http";
-import { Server } from "socket.io";
 import { faker } from '@faker-js/faker'
 import cors from "cors";
 import { scheduleJob } from 'node-schedule';
@@ -10,9 +9,7 @@ import {
   deleteStaleDBItems,
   isRoomInDB,
   addRoomToDB,
-  addUserToRoom,
   getRoomScore,
-  getUserFromDB,
   updatePlayerScore,
   createNewHole,
   validateUserIsInRoom,
@@ -38,52 +35,6 @@ app.use(morgan('tiny'))
 app.use(express.json())
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: ["http://localhost:3000", "http://localhost:5173"]
-  }
-});
-
-const roomIo = io.of("/api/room")
-roomIo.on("connect", (socket) => {
-    // validate room id on joining
-    socket.on("ping", () => {
-        console.log(`Received ping from ${socket.id}`)
-        socket.emit("pong")
-    })
-    socket.on('join-room', async ({ roomName, username}, onError ) => {
-      // TODO: Add error handling, e.g. if username already exist?
-      const promises = []
-
-      let validatedRoom: string
-      let validatedUserName: string
-
-      try {
-        validatedRoom = z.string().parse(roomName.toLowerCase())
-        validatedUserName = z.string().parse(username)
-      } catch (err) {
-        onError(JSON.stringify(err, Object.getOwnPropertyNames(err)))
-        return
-      }
-
-      // Creates a user and adds them to a room
-      const user = await getUserFromDB(validatedUserName)
-
-      if (!(await isRoomInDB(validatedRoom))) promises.push(addRoomToDB(validatedRoom))
-      promises.push(addUserToRoom(user ?? validatedUserName, validatedRoom))
-      promises.push(socket.join(validatedRoom))
-      await Promise.allSettled(promises)
-
-      socket.emit("Joined room", roomName)
-      console.log(`${socket.id} join room: ${roomName}`)
- 
-    })
-    console.log(socket.id)
-
-    socket.on('update-state', (state) => {
-        socket.broadcast.emit("update-state", state)
-    })
-});
 
 app.get('/', (_req, res) => res.json({ message: "hello world" }))
 
