a0228eecbc7688716123d671561c44b1153e8f8f
Sachin Somia <sachinsomia@gmail.com>
2023-03-18 17:05:16 +0100

    Feat: Get front end URL from env vars

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,8 +26,10 @@ const port = process.env.PORT || 8080
 
 const app = express();
 
+const FRONTEND_URL = process.env.FRONTEND_URL ?? "http://localhost:5173"
+
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: FRONTEND_URL,
 }))
 
 app.use(morgan('tiny'))
